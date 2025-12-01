30c18cb3c69f3f305f5a6f442b8ba286b33b6511
Veeramanikandan R <r.veeramanikandany216@gmail.com>
2025-12-01 14:55:34 +0530

    initial_commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  decrement,
-  fetchUser,
-  increaseByValue,
-  increment,
-} from "./redux/reducers/counterSlice";
+import React from "react";
 
 const App = () => {
-  let count = useSelector((state) => state.counter);
-  count = count.count;
-  const users = useSelector((state) => state.counter.users);
-  console.log("count", count);
-  console.log("users", users);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchUser());
-  }, []);
-  return (
-    <div>
-      <div>Count: {count}</div>
-      <button onClick={() => dispatch(increment())}>increment</button>
-      <button onClick={() => dispatch(decrement())}>decrement</button>
-      <button onClick={() => dispatch(increaseByValue(2))}>inc by 2</button>
-      <div>Users</div>
-      <div>Loading: {users.loading}</div>
-      <div>Error: {users.error}</div>
-      <div>Users: {JSON.stringify(users.data)}</div>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
